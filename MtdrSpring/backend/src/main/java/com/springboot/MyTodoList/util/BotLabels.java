839d20dd57ad58728202336dd0ce2b3d0d4745a0
Daniel Eden Wynter González <A01645202@tec.mx>
2026-04-27 11:11:11 -0600

    Telegram bot allows you to see listed tasks and priority

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotLabels.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotLabels.java
@@ -1,15 +1,9 @@
 package com.springboot.MyTodoList.util;
 
 public enum BotLabels {
-	
-	SHOW_MAIN_SCREEN("Show Main Screen"), 
-	HIDE_MAIN_SCREEN("Hide Main Screen"),
-	LIST_ALL_ITEMS("List All Items"), 
-	ADD_NEW_ITEM("Add New Item"),
-	DONE("DONE"),
-	UNDO("UNDO"),
-	DELETE("DELETE"),
-	MY_TODO_LIST("MY TODO LIST"),
+
+	SHOW_MAIN_SCREEN("Show Main Screen"),
+	VIEW_TASKS("📋 Ver Tasks"),
 	DASH("-");
 
 	private String label;
